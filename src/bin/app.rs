dd9cdfd5d6eef49a89618e80fa9d772f7836fd1f
Andrei Belov <andrei.belov.fi@outlook.com>
2023-05-25 19:00:45 +0300

    Working prototype You can pass file of dir to check one or all APIs endpoints.

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -1,8 +1,11 @@
-use bellhop::files::*;
-use bellhop::parser::parser::{Method, Request};
-use bellhop::parser::*;
+use bellhop::data::files::configs::{get_cfg, get_files};
+use bellhop::domain::Request;
+use bellhop::model::request;
 use std::error::Error;
+use std::string::ToString;
 use structopt::StructOpt;
+
+const DEFAULT_ENV: &str = "default";
 
 // #[derive(StructOpt, Debug)]
 // enum Command {
@@ -24,57 +27,39 @@ use structopt::StructOpt;
 struct Opt {
     // #[structopt(subcommand)]
     // command: Command,
-    #[structopt(default_value = "http://127.0.0.1:8000", env = "CLIPSTASH_ADDR")]
+    #[structopt(default_value = "http://127.0.0.1:8000", env = "BELLHOP_ADDR")]
     addr: String,
+    #[structopt(short, long, help = "what env setup should be used")]
+    env: Option<String>,
     #[structopt(short, long, help = "location of json file to run")]
     file: Option<String>,
     #[structopt(short, long, help = "location of dir with json files to run")]
     dir: Option<String>,
 }
 
-fn make_request(req: Request) -> Result<(), Box<dyn Error>> {
-    let client = reqwest::blocking::Client::builder().build()?;
-    match req.method {
-        Method::GET => {
-            let mut request = client.get(&req.host);
-            let r = request.json(&req.body).send()?;
-            println!("{:#?}", r)
-        }
-
-        Method::POST => {
-            let mut request = client.post(&req.host);
-            let r = request.json(&req.body).send()?;
-            println!("{:#?}", r)
-        }
-        _ => println!("wrong argument"),
+fn run(opt: Opt) -> Result<(), Box<dyn Error>> {
+    let env;
+    if opt.env.is_some() {
+        env = opt.env.unwrap();
+    } else {
+        env = DEFAULT_ENV.to_string();
     }
 
-    // if req.method.to_lowercase() == "post" {
-    //     let mut request = client.post(&req.host);
-    //     let r = request.json(&req.body).send()?;
-    //     println!("{:#?}", r)
-    // }
-    // if req.method == "get" {
-    //     let mut request = client.get(&req.host);
-    //     let r = request.json(&req.body).send()?;
-    //     println!("{:#?}", r)
-    // }
-    Ok(())
-}
-
-fn run(opt: Opt) -> Result<(), Box<dyn Error>> {
     if opt.file.is_some() {
-        let path = files::get_cfg(&opt.file.unwrap_or_else(|| "undefined".to_string()))?;
-        let req = parser::get_cfg_data(path.as_path());
-        make_request(req).expect("err");
+        let path = get_cfg(&opt.file.unwrap_or_else(|| "undefined".to_string()))?;
+        let req = request::deserialize_data(path.as_path(), &env);
+        match req.do_request() {
+            Ok(()) => println!("Done"),
+            Err(e) => println!("Request filed: {:?}", e),
+        }
     } else if opt.dir.is_some() {
         let files = &mut vec![];
-        let path = files::get_cfg(&opt.dir.unwrap_or_else(|| "undefined".to_string()))?;
-        let res = files::get_files(&path, files);
+        let path = get_cfg(&opt.dir.unwrap_or_else(|| "undefined".to_string()))?;
+        let res = get_files(&path, files);
         match res {
             Ok(_) => {
                 for file in files {
-                    let req = parser::get_cfg_data(file.as_path());
+                    let req = request::deserialize_data(file.as_path(), &env);
                 }
             }
             Err(e) => println!("{:#?}", e),
